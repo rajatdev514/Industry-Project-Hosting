d72830f3dc762ff63e6d1ba456699f97674df800
Rajat Mahajan <150473637+rajatdev514@users.noreply.github.com>
2024-10-23 23:05:54 +0530

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,16 +1,11 @@
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_API,
+      target: process.env.REACT_APP_API, // Points to your backend
       changeOrigin: true,
-      secure: false,
-      headers: {
-        Host: 'localhost',
-      },
     })
   );
 };
